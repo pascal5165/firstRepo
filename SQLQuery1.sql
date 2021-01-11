d6ce9ca048f4a0561d362a30eba7f044a3b6165f
pascal5165 <64109704+pascal5165@users.noreply.github.com>
2021-01-11 13:05:52 +0000

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+create database pascal
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+select * from Persons
